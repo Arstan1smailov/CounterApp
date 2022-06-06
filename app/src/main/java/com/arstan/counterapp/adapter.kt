04f1ee5d23e163e6c76305dcc9cf7e49e5203d7a
Arstan1smailov <drkskillet@gmail.com>
2022-06-06 12:01:42 +0600

    a

---
+++ app/src/main/java/com/arstan/counterapp/adapter.kt
@@ -0,0 +1,24 @@
+package com.arstan.counterapp
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.arstan.counterapp.databinding.ItemHistoryBinding
+
+class adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class ViewHolder(var binding: ItemHistoryBinding, viewHolder: ViewHolder) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ???, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+
+    }
+}
--- app/src/main/java/com/arstan/counterapp/ui/notifications/HistoryFragment.kt
+++ app/src/main/java/com/arstan/counterapp/ui/notifications/HistoryFragment.kt
@@ -3,10 +3,6 @@ package com.arstan.counterapp.ui.notifications
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.arstan.counterapp.Base.BaseFragment
 import com.arstan.counterapp.ViewModel.ViewModel
 import com.arstan.counterapp.databinding.FragmentHistoryBinding
@@ -16,5 +12,10 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding, ViewModel>() {
         binding = FragmentHistoryBinding.inflate(inflater)
         return binding
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 
 }
